4fd117f1ebdc53b4baa70d8c05c6a67cb2115c77
Peter Duchovni <peter.duchovni@tqtezos.com>
2021-09-16 15:19:35 -0400

    rustgen: scaffolding for boolean and option
    Extends implementation in tcc/rust/kernel library to facilitate
    future handling of boolean value parsing, as well as laying the
    groundwork required for automatically derived Option<T> codec
    implementations.
    
    Enriches and refactors the ParseError enumerator to allow for
    cleaner boilerplate at callsite, and to allow extensibility in
    future revisions of the model.

--- rust_runtime/src/byteparser.rs
+++ rust_runtime/src/byteparser.rs
@@ -1,12 +1,65 @@
-use std::num::ParseIntError;
 use std::fmt;
+use std::num::ParseIntError;
 
 #[derive(Debug, Clone)]
-pub struct BufferOverflow(usize,usize,usize);
+pub enum InternalErrorKind {
+    ConsumeLengthMismatch { expected: usize, actual: usize },
+}
 
-impl fmt::Display for BufferOverflow {
+impl InternalErrorKind {
+    fn length_mismatch(expected: usize, actual: usize) -> Self {
+        Self::ConsumeLengthMismatch { expected, actual }
+    }
+}
+
+#[derive(Debug, Clone)]
+pub enum ParseError {
+    InternalError(InternalErrorKind),
+    BufferOverflow {
+        buflen: usize,
+        offset: usize,
+        requested: usize,
+    },
+    InvalidBoolean(u8),
+}
+
+impl ParseError {
+    fn internal(kind: InternalErrorKind) -> Self {
+        Self::InternalError(kind)
+    }
+}
+
+impl fmt::Display for InternalErrorKind {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "target position at _{}_+<{}> overflows [{}]", self.0, self.1, self.2)
+        match self {
+            InternalErrorKind::ConsumeLengthMismatch { expected, actual } => {
+                write!(
+                    f,
+                    "consume({}) returned slice of length {}",
+                    expected, actual
+                )
+            }
+        }
+    }
+}
+
+impl fmt::Display for ParseError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            ParseError::BufferOverflow {
+                buflen,
+                offset,
+                requested,
+            } => {
+                write!(f, "cannot increment offset by {} bytes (currently at byte {} in buffer of length {})", requested, offset, buflen)
+            }
+            ParseError::InternalError(err) => {
+                write!(f, "internal error ({})", err)
+            }
+            ParseError::InvalidBoolean(byte) => {
+                write!(f, "expected boolean := (0xff | 0x00), got 0x{:02x}", byte)
+            }
+        }
     }
 }
 
@@ -29,14 +82,14 @@ impl Bytes {
                 .map(|wix| to_word(&s[wix..wix + 2]))
                 .collect();
             match words {
-                Result::Ok(v) => { return Ok(Bytes(v)) }
-                Result::Err(e) => { return Err(format!("Bytes::from: to_word failed with context {:?}", e)) }
+                Ok(v) => return Ok(Bytes(v)),
+                Err(e) => return Err(format!("Bytes::from: to_word failed with context {:?}", e)),
             }
         }
     }
 
     unsafe fn get_slice(&self, ix: usize, len: usize) -> &[u8] {
-        &self.0[ix..ix+len]
+        &self.0[ix..ix + len]
     }
 
     fn len(&self) -> usize {
@@ -44,53 +97,65 @@ impl Bytes {
     }
 }
 
-
 pub struct ByteParser {
-    _buf : Bytes,
-    _offset : std::cell::Cell<usize>
+    _buf: Bytes,
+    _offset: std::cell::Cell<usize>,
 }
 
 impl ByteParser {
     pub fn parse(s: &str) -> Result<Self, String> {
         match Bytes::from(s) {
-            Result::Ok(_buf) => {
-                Ok(ByteParser {
-                    _buf,
-                    _offset: std::cell::Cell::new(0usize),
-                })
-            }
-            Result::Err(e) => {
-                Err(e)
-            }
+            Ok(_buf) => Ok(ByteParser {
+                _buf,
+                _offset: std::cell::Cell::new(0usize),
+            }),
+            Err(e) => Err(e),
         }
     }
 
-    fn consume(&self, nbytes: usize) -> Result<&[u8],BufferOverflow> {
-        let cur : usize = self._offset.get();
+    fn consume(&self, nbytes: usize) -> Result<&[u8], ParseError> {
+        let cur: usize = self._offset.get();
 
         let tgt = cur + nbytes;
 
         if cur + nbytes > self._buf.len() {
-            Err(BufferOverflow(cur, nbytes, self._buf.len()))
+            Err(ParseError::BufferOverflow {
+                offset: cur,
+                requested: nbytes,
+                buflen: self._buf.len(),
+            })
         } else {
-            Ok(
-                unsafe {
-                    let ret = self._buf.get_slice(cur, nbytes);
-                    self._offset.set(tgt);
-                    ret
-                })
+            Ok(unsafe {
+                let ret = self._buf.get_slice(cur, nbytes);
+                self._offset.set(tgt);
+                ret
+            })
         }
     }
 
-    pub fn get_uint8(&self) -> Result<u8, BufferOverflow> {
+    pub fn get_uint8(&self) -> Result<u8, ParseError> {
         match self.consume(1) {
-            Result::Ok(ws) => {
-                assert_eq!(ws.len(), 1);
-                Ok(ws[0])
-            }
-            Result::Err(e) => {
-                Err(e) 
-            }
+            Ok(bytes) => match bytes {
+                &[byte] => Ok(byte),
+                other => {
+                    Err(ParseError::internal(InternalErrorKind::length_mismatch(1, other.len())))
+                }
+            },
+            Err(e) => Err(e),
+        }
+    }
+
+    pub fn get_bool(&self) -> Result<bool, ParseError> {
+        match self.consume(1) {
+            Ok(bytes) => match bytes {
+                &[0x00] => Ok(false),
+                &[0xff] => Ok(true),
+                &[byte] => Err(ParseError::InvalidBoolean(byte)),
+                other => {
+                    Err(ParseError::internal(InternalErrorKind::length_mismatch(1, other.len())))
+                }
+            },
+            Err(e) => Err(e),
         }
     }
 }
--- rust_runtime/src/conv.rs
+++ rust_runtime/src/conv.rs
@@ -13,3 +13,57 @@ pub trait Encode<U> {
 pub trait Decode<T> {
     fn decode(&self) -> T;
 }
+
+/*
+impl<T> ToBinary for Option<T> where T: ToBinary {
+    fn bin_encode(&self) -> String {
+        match &self {
+            Some(val) => T::bin_encode(val),
+            None => String::from("00"),
+        }
+    }
+}
+
+impl<T> FromBinary for Option<T> where T: FromBinary {
+    fn bin_decode(inp: &str) -> Self {
+        match crate::byteparser::ByteParser::parse(inp) {
+            Ok(p) => {
+                match p.get_bool() {
+                    Ok(true) => Some(T::bin_decode(&inp[1..])),
+                    Ok(false) => None,
+                    Err(err) => std::panic!("{}", err),
+                }
+            },
+            Err(err) => std::panic!("{}", err),
+        }
+    }
+}
+
+impl<T> Encode<String> for Option<T> where T: Encode<String> {
+    fn encode(&self) -> String { 
+        match self {
+            Some(val) => val.encode(),
+            None => String::from("00"),
+        }
+    }
+}
+
+impl<T, U> Decode<Option<T>> for U where
+U: Decode<T> + Decode<bool> {
+    fn decode(&self) -> Option<T> {
+        if self.decode() {
+            // this has not been implemented sufficiently to define
+        }
+        match crate::byteparser::ByteParser::parse(inp) {
+            Ok(p) => {
+                match p.get_bool() {
+                    Ok(true) => Some(T::bin_decode(&self[1..])),
+                    Ok(false) => None,
+                    Err(err) => std::panic!("{}", err),
+                }
+            },
+            Err(err) => std::panic!("{}", err),
+        }
+    }
+}
+*/
