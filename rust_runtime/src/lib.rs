d26abac09c536aab93901386d950dae8c5668fb6
Peter Duchovni <peter.duchovni@tqtezos.com>
2021-10-19 17:55:32 -0400

    rustgen: adds u30 and i31 to import/export
    Adds u30 and i31 type aliases to rust_runtime crate export in lib.rs
    
    Adds import of u30 and i31 by default to the preamble of all
    generated codecs.
    
    TODO: figure out which imports are necessary in generation rather than
    including the union of all potentially needed imports
    by default.

--- rust_runtime/src/lib.rs
+++ rust_runtime/src/lib.rs
@@ -3,6 +3,7 @@ mod parse;
 mod prim;
 mod int;
 
+pub use crate::int::{u30,i31};
 pub use crate::conv::{Decode, Encode};
 pub use crate::parse::byteparser::{ToParser, ByteParser};
 pub use crate::parse::hexstring::HexString;
