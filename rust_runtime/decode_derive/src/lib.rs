481c623b3007675739ce2dabb5ea57750d7220c9
Peter Duchovni <peter.duchovni@nomadic-labs.com>
2021-11-18 12:16:47 -0500

    rustgen: derive macro prototypes
    Adds new crates for derive macros for boilerplate:
     - `encode_derive`: Encode (Unit and Tuple structs)
     - `decode_derive`: Decode (all structs, we think)
     - `estimable_derive`: unimplemented
    
    Adds more utility functions/operators to Generator.Ctxt
    
    Introduces attribute annotations to Rust.Ast, deprecating explicit
    DTestFunction in favor of
    smart-constructor `testfn`
    that uses `DFunction` as its base
    
    Adds constructor `DAnnot` to Rust.Ast.Decl
    
    Adds indicator functions to Generator.Gen
    to decide between type-alias/newtype,
    derive-macro/compiled boilerplate of
    Decode/Encode.
    
    TODO: settle on a more concrete design
    for Estimable et al.
    TODO: add derive or autogen for Estimable and its refinement traits

---
+++ rust_runtime/decode_derive/src/lib.rs
@@ -0,0 +1,52 @@
+extern crate proc_macro;
+
+use proc_macro::TokenStream;
+use quote::quote;
+use syn;
+
+#[proc_macro_derive(Decode)]
+pub fn decode_derive(input: TokenStream) -> TokenStream {
+    let ast = syn::parse(input).unwrap();
+
+    impl_decode(&ast)
+}
+
+fn impl_decode(ast: &syn::DeriveInput) -> TokenStream {
+    let name = &ast.ident;
+    let gen = match &ast.data {
+        syn::Data::Enum(_) => unimplemented!(),
+        syn::Data::Union(_) => unimplemented!(),
+        syn::Data::Struct(syn::DataStruct { fields, .. }) => match fields {
+            syn::Fields::Unit => {
+                quote! {
+                    impl Decode for #name {
+                        fn parse<P: Parser>(_: &mut P) -> Self {
+                            Self()
+                        }
+                    }
+                }
+            },
+            syn::Fields::Unnamed(syn::FieldsUnnamed { unnamed, ..}) => {
+                let ty = unnamed.iter().map(|x| &x.ty);
+                quote! {
+                    impl Decode for #name {
+                        fn parse<P: Parser>(p: &mut P) -> Self {
+                            Self(#( <#ty as Decode>::parse(p) ),*)
+                        }
+                    }
+                }
+            },
+            syn::Fields::Named(syn::FieldsNamed { named, ..}) => {
+                let (fname, ty) : (Vec<&syn::Ident>, Vec<&syn::Type>) = named.iter().map(|x| (x.ident.as_ref().unwrap(), &x.ty)).unzip();
+                quote! {
+                    impl Decode for #name {
+                        fn parse<P: Parser>(p: &mut P) -> Self {
+                            Self { #( #fname: <#ty as Decode>::parse(p) ),* }
+                        }
+                    }
+                }
+            }
+        },
+    };
+    gen.into()
+}
---
+++ rust_runtime/encode_derive/src/lib.rs
@@ -0,0 +1,41 @@
+extern crate proc_macro;
+
+use proc_macro::TokenStream;
+use quote::quote;
+use syn;
+
+#[proc_macro_derive(Encode)]
+pub fn encode_derive(input: TokenStream) -> TokenStream {
+    let ast = syn::parse(input).unwrap();
+
+    impl_encode(&ast)
+}
+
+fn impl_encode(ast: &syn::DeriveInput) -> TokenStream {
+    let name = &ast.ident;
+    let gen = match &ast.data {
+        syn::Data::Enum(_) => unimplemented!(),
+        syn::Data::Union(_) => unimplemented!(),
+        syn::Data::Struct(syn::DataStruct { fields, .. }) => match fields {
+            syn::Fields::Unit => {
+                quote! {
+                    impl Encode for #name {
+                        fn write(&self, _: &mut Vec<u8>) {}
+                    }
+                }
+            },
+            syn::Fields::Unnamed(syn::FieldsUnnamed { unnamed, ..}) => {
+                let i = (0..unnamed.len()).map(syn::Index::from);
+                quote! {
+                    impl Encode for #name {
+                        fn write(&self, buf: &mut Vec<u8>) {
+                            #( self.#i.write(buf); )*
+                        }
+                    }
+                }
+            },
+            syn::Fields::Named(syn::FieldsNamed { ..}) => unimplemented!(),
+        },
+    };
+    gen.into()
+}
