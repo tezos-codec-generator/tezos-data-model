f317969746ddd92b91c3116cc61a05186b265568
Peter Duchovni <peter.duchovni@nomadic-labs.com>
2021-11-30 12:49:07 -0500

    rustgen: non-macro impl for products
    Adds case logic for handling generated (non-macro)
    implementations of Encode and Decode
    for Product types
    
    Introduces more modular system for specifying generator
    behavior through Gen.Conf module
    
    Refactors Ctxt module to include metadata
    on derive-macro imports, which are used
    in ast.ml to infer what
    derive crates to include in preamble.
    
    Introduces more consistent approach for
    regular import list
    from Ctxt.t, un-hardcoding
    Encode, Decode and forcing their
    annotation in the Gen module.

--- rust_runtime/src/builder/memo.rs
+++ rust_runtime/src/builder/memo.rs
@@ -1,10 +1,13 @@
+#[allow(unused_imports)]
 use super::{Builder, TransientBuilder};
 
+#[allow(dead_code)]
 pub union Metadata<'a> {
     field_name: &'a str,
     tuple_pos: usize,
 }
 
+#[allow(dead_code)]
 pub struct Hint<'a> {
     width: usize,
     extra: Metadata<'a>
